4bb0c09a17bf02e0ef46c86c08fb65724d1956eb
Sky Xu <skyrocks88xmz@gmail.com>
2017-09-22 13:10:49 -0700

    finish all helper function

--- Hangman_Project/ps3_hangman.py
+++ Hangman_Project/ps3_hangman.py
@@ -17,11 +17,11 @@ def isWordGuessed(secretWord, lettersGuessed):
       False otherwise
     '''
     for i in range(0, len(secretWord)):
-      if secretWord[i] in lettersGuessed:
-        lettersGuessed.remove(secretWord[i])
-      else:
+      if not secretWord[i] in lettersGuessed:
         return False
-    return True
+    else:
+      return True
+
 
 
 
@@ -35,14 +35,16 @@ def getGuessedWord(secretWord, lettersGuessed):
     in the word that the user has not yet guessed, shown an _ (underscore) instead.
     '''
     # FILL IN YOUR CODE HERE...
-    print_letters = "_"*len(secretWord)
-    for i in range(0, len(secretWord)):
-      for n in range(0, len(lettersGuessed)):
-        if lettersGuessed[n] == secretWord[i]:
-          print_letters[i-1:i].replace("_", "lettersGuessed")
-          return print_letters
-        else:
-          return print_letters
+    print(lettersGuessed)
+    msg = ""
+    for i in secretWord:
+      if i in lettersGuessed:
+        msg += i
+      else:
+        msg += " _ "
+    return msg
+
+
 
 
 
@@ -56,7 +58,7 @@ def getAvailableLetters(lettersGuessed):
     '''
     for i in range(0, len(lettersGuessed)):
      if lettersGuessed[i] in secretWord:
-      return secretWord.remove(lettersGuessed[i])
+      return secretWord.replace("lettersGuessed[i]", "")
      else:
       return ""
 
@@ -79,26 +81,22 @@ def hangman(secretWord):
       partially guessed word so far, as well as letters that the
       user has not yet guessed.
     '''
-    guessLeft = 8
+    guessLeft = len(secretWord) + 2
     print("start the game, the number of secretWord letters is", len(secretWord))
+    print("secret word is ", secretWord)
+    lettersGuessed = []
     while guessLeft > 0:
       guess = input("guess one letter")
-      lettersGuessed = []
       lettersGuessed.append(guess)
       getAvailableLetters(lettersGuessed)
-      if lettersGuessed.count(guess) > 1:
-        print("oops you have already guessed the word")
-      elif guess in secretWord:
-        print str("GoodGuess!")+getGuessedWord(secretWord, lettersGuessed)
-        if isWordGuessed(secretWord, lettersGuessed) == true:
-          return str("Congrats, you won!")
-          break
-      else:
-       guessLeft -= 1
-       print str("this is not the right word")+getGuessedWord(secretWord, lettersGuessed)
-
+      guessLeft -= 1
+      print("all guess try is here", getGuessedWord(secretWord, lettersGuessed))
+      print('&&&&&&&', guessLeft)
+      if isWordGuessed(secretWord, lettersGuessed) == True:
+        print("Congrats, you won!")
+        break
     else:
-      return str("sorry your are out of guess, the secret word is " + str(secretWord))
+      return print("sorry your are out of guess, the secret word is ", secretWord)
 
 secretWord = loadWord()
 hangman(loadWord())
